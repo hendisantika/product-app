d10145793a97a6e1c20d0b39b26d995d4999acab
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 06:13:59 +0700

    Add deleteById in ProductController 🫸🌀✏️📗🐧🐳

--- backend/src/main/java/id/my/hendisantika/backend/controller/ProductController.java
+++ backend/src/main/java/id/my/hendisantika/backend/controller/ProductController.java
@@ -3,12 +3,15 @@ package id.my.hendisantika.backend.controller;
 import id.my.hendisantika.backend.model.Product;
 import id.my.hendisantika.backend.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -48,5 +51,11 @@ public class ProductController {
     @GetMapping("/{id}")
     public Product findById(@PathVariable Long id) {
         return productService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
     }
 }
