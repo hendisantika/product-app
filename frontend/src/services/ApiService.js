431997318c3669b93b01f8a92d3052500f748bef
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 06:32:38 +0700

    Add ApiService.js 🫸🌀✏️📗🐧🐳

---
+++ frontend/src/services/ApiService.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const baseURL = process.env.REACT_APP_API_BASE_URL;
+
+const apiUrl = `${baseURL}/api/products`;
+
+export const getProducts = async () => {
+    try {
+        const response = await axios.get(apiUrl);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const createProduct = async (product) => {
+    try {
+        const response = await axios.post(apiUrl, product);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const getProductById = async (id) => {
+    try {
+        const response = await axios.get(`${apiUrl}/${id}`);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const updateProductById = async (id, product) => {
+    try {
+        const response = await axios.put(`${apiUrl}/${id}`, product);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const deleteProductById = async (id) => {
+    try {
+        const response = await axios.delete(`${apiUrl}/${id}`);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+}
