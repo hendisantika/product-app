633f6c5a892d329812cea6caaecef86a9f3745f3
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 06:31:12 +0700

    Add ProductContext.js 🫸🌀✏️📗🐧🐳

---
+++ frontend/src/context/ProductContext.js
@@ -0,0 +1,32 @@
+import React, {createContext, useState} from 'react';
+
+export const ProductContext = createContext();
+
+export const ProductListProvider = ({children}) => {
+    const [products, setProducts] = useState([]);
+    const [product, setProduct] = useState({});
+
+    const updateProducts = (products) => {
+        setProducts(products)
+    }
+
+    const addProduct = (product) => {
+        setProducts([...products, product]);
+    }
+
+    const updateProduct = (product) => {
+        setProduct(product);
+    }
+
+    const removeProductById = (id) => {
+        const newProducts = products.filter((product) => product.id !== id);
+        setProducts(newProducts);
+    }
+
+    return (
+        <ProductContext.Provider
+            value={{products, product, updateProducts, updateProduct, removeProductById, addProduct}}>
+            {children}
+        </ProductContext.Provider>
+    );
+}
