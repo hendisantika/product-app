8623ef187c7dd65c4582d2cc91f908cb90fc1223
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 06:08:59 +0700

    Add updateById in ProductService 🫸🌀✏️📗🐧🐳

--- backend/src/main/java/id/my/hendisantika/backend/service/ProductService.java
+++ backend/src/main/java/id/my/hendisantika/backend/service/ProductService.java
@@ -4,7 +4,9 @@ import id.my.hendisantika.backend.model.Product;
 import id.my.hendisantika.backend.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,5 +26,15 @@ public class ProductService {
 
     public Product save(Product product) {
         return productRepository.save(product);
+    }
+
+    public Product updateById(Long id, Product product) {
+        Product managedProduct = productRepository.findById(id).orElseThrow(() -> new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "entity not found"));
+        managedProduct.setTitle(product.getTitle());
+        managedProduct.setQuantity(product.getQuantity());
+        managedProduct.setPrice(product.getPrice());
+
+        return this.save(managedProduct);
     }
 }
