94e66da1e1dbce4a98875e7a34415e4639276b04
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 06:43:38 +0700

    Modify App.js🫸🌀✏️📗🐧🐳

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import CreateProductForm from './components/CreateProductForm'
+import NavBar from "./components/NavBar";
+import ProductList from "./components/ProductList";
+import Products from "./components/Products";
+import {ProductListProvider} from "./context/ProductContext";
+import ProductDetail from "./components/ProductDetail";
+import UpdateProductForm from './components/UpdateProductForm';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Router>
+            <ProductListProvider>
+                <div className="container">
+                    <NavBar/>
+                    <hr/>
+                    <Routes>
+                        <Route path="/new" element={<CreateProductForm/>}/>
+                        <Route path="/" element={<Products/>}>
+                            <Route index element={<ProductList/>}/>
+                            <Route path=":id" element={<ProductDetail/>}/>
+                            <Route path=":id/edit" element={<UpdateProductForm/>}/>
+                        </Route>
+                    </Routes>
+                </div>
+            </ProductListProvider>
+        </Router>
     );
 }
 
