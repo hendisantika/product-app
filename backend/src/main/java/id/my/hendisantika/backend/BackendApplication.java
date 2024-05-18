adb3a9ff33a165a85b33baa06648d73d86c4f5ec
hendisantika <hendisantika@yahoo.co.id>
2024-05-19 06:11:25 +0700

    Add initData in Main Class 🫸🌀✏️📗🐧🐳

--- backend/src/main/java/id/my/hendisantika/backend/BackendApplication.java
+++ backend/src/main/java/id/my/hendisantika/backend/BackendApplication.java
@@ -1,7 +1,11 @@
 package id.my.hendisantika.backend;
 
+import id.my.hendisantika.backend.model.Product;
+import id.my.hendisantika.backend.service.ProductService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +14,20 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner initData(ProductService productService) {
+        return args -> {
+            Product product = new Product();
+            product.setTitle("iPhone");
+            product.setPrice(1200D);
+            product.setQuantity(99);
+            productService.save(product);
+
+            Product product2 = new Product();
+            product2.setTitle("Blackberry");
+            product2.setPrice(1100D);
+            product2.setQuantity(55);
+            productService.save(product2);
+        };
+    }
 }
